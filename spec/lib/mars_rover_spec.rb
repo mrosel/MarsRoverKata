06fa2f9c4f84e0eca0d8bcb8adb17b2f8d0f0341
pestovpvl <pestovpvl@mail.ru>
2019-12-17 21:47:02 -0500

    Created test for checkes whether a rover class responds to the rover move

--- spec/lib/mars_rover_spec.rb
+++ spec/lib/mars_rover_spec.rb
@@ -1,10 +1,16 @@
-require '../lib/mars_rover.rb'
+require '/Users/myfoulder/Desktop/test-task/MarsRoverKata/lib/mars_rover.rb'
 require '/Users/myfoulder/Desktop/test-task/MarsRoverKata/spec/spec_helper.rb'
 
 RSpec.describe MarsRover do
-  # Checks whether rover create
+  # Checks whether MarsRover create
   it 'should respond to: Rover move forward (North)' do 
     rover = MarsRover.new
     expect(rover).to be_kind_of(MarsRover)
+  end
+
+#Checkes whether a rover class responds to the rover move
+  it 'rovers' do
+    rover = MarsRover.new
+    expect(rover).to respond_to(:move)
   end
 end
