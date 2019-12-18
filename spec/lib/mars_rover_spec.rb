a7be6b6a09cb0739eee22787e8cfe939203df6ce
pestovpvl <pestovpvl@mail.ru>
2019-12-17 21:21:12 -0500

    Class Creation Check

--- spec/lib/mars_rover_spec.rb
+++ spec/lib/mars_rover_spec.rb
@@ -1,6 +1,10 @@
-require './lib/mars_rover'
-require 'spec_helper'
+require '../lib/mars_rover.rb'
+require '/Users/myfoulder/Desktop/test-task/MarsRoverKata/spec/spec_helper.rb'
 
 RSpec.describe MarsRover do
-  it { expect(true) }
+  # Checks whether rover create
+  it 'should respond to: Rover move forward (North)' do 
+    rover = MarsRover.new
+    expect(rover).to be_kind_of(MarsRover)
+  end
 end
